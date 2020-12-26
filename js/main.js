2e288feadf537b998ea85b0dbe1e1874b0748553
salma badr <salmabadr27795@gmail.com>
2020-12-26 17:23:46 +0200

    featured work

--- js/main.js
+++ js/main.js
@@ -6,4 +6,14 @@ $(function () {
 	var sliderH = winH - (upperBarH + navH);
 	$(".slider").height(sliderH);
 	$(".carousel-item").height(sliderH);
+
+	$(".featured-work ul li").on("click", function () {
+		$(this).addClass("active").siblings().removeClass("active");
+		if ($(this).data("class") === "all") {
+			$(".shuffle-imgs .col-sm").css("opacity", 1);
+		} else {
+			$(".shuffle-imgs .col-sm").css("opacity", ".08");
+			$($(this).data("class")).parent().css("opacity", 1);
+		}
+	});
 });
